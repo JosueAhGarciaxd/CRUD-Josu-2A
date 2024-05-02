45cf8c488ff4146ccc086b07e6e58f6bcf230578
Josué Hernández <20230098@ricaldone.edu.sv>
2024-05-02 10:04:46 -0600

    Commit 3

--- app/src/main/java/josue/a/crudjosue2a/MainActivity.kt
+++ app/src/main/java/josue/a/crudjosue2a/MainActivity.kt
@@ -1,10 +1,16 @@
 package josue.a.crudjosue2a
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import modelo.ClaseConexion
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //1-Mandamos a llamar a todos los elementos de la pantalla
+        val txtNombre = findViewById<EditText>(R.id.txtNombre)
+        val txtPrecio = findViewById<EditText>(R.id.txtPrecio)
+        val txtCantidad = findViewById<EditText>(R.id.txtCantidad)
+        val btnAgregar = findViewById<Button>(R.id.btnAgregar)
+
+        //2- Programar el boton
+        btnAgregar.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO){
+
+                //Guardad datos
+                //1-Crea un objeto de la clase Conexion
+                val ClaseConexion = ClaseConexion().cadenaConexion()
+
+                //2- Creo una variable que contenga un PrepareStatement
+                val addProducto = ClaseConexion?.prepareStatement("insert into tbProducto(nombreProducto, precio, cantidad) values(?,?,?)")
+                
+            }
         }
     }
 }
